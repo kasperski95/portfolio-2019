8f573b2e47935bb9b90077eb09a2def6b1b643de
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2019-03-12 20:20:32 +0100

    Add mail support

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import blueGrey from '@material-ui/core/colors/blueGrey';
-import { createMuiTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-
-
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import blueGrey from '@material-ui/core/colors/blueGrey'
+import { createMuiTheme } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import axios from 'axios'
 
 
 export default class Contact extends Component {
+	state={
+		submitLabel: 'Send',
+		submitButtonDisabled: false,
+	}
+
 	constructor(props) {
 		super(props);
 
@@ -37,18 +44,39 @@ export default class Contact extends Component {
 		return this.defaultTheme;
 	}
 
+	
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.setState({submitLabel: 'Sending...', submitButtonDisabled: true});
+		const data = new FormData(e.target);
+		axios.post('/mail.php', data)
+			.then(result => {				
+				if (result.status === 200) {
+					this.setState({submitLabel: 'Sent', submitButtonDisabled: true});
+				} 
+			})
+			.catch(result => {
+				alert("Message couldn't been sent. Check captcha.");
+				this.setState({submitLabel: 'Send', submitButtonDisabled: false});
+			});
+	}
+
+
   render() {
 		const theme = this.getTheme();
 
     return (
-			<form style={{display: `flex`, justifyContent: 'center', width: `100%`, padding: '0em 1em', boxSizing: `border-box`}}>
-				<Card style={{backgroundColor: theme.palette.background.paper, color: theme.palette.text.main, boxSizing: `border-box`, overflow: 'hidden', maxWidth: '25em', margin: `5% 0em 1em 0em`, width: `100%`, padding: '2em 1em', textAlign: `right`, height: `auto`}}>
-					<TextField label="E-mail" name="email" required style={inputStyle} />
-					<TextField label="Message" multiline required style={inputStyle} />
-					{/* <div style={{height: `5em`, marginBottom: `1em`, border: `1px solid gray`}} /> */}
-					<Button variant='contained' color='primary' type="submit">Send</Button>
-				</Card>
-			</form>
+				<form method="POST" onSubmit={(e) => this.handleSubmit(e)} style={{display: `flex`, justifyContent: 'center', width: `100%`, padding: '0em 1em', boxSizing: `border-box`}}>
+					<Card style={{backgroundColor: theme.palette.background.paper, color: theme.palette.text.main, boxSizing: `border-box`, overflow: 'hidden', maxWidth: '25em', margin: `5% 0em 1em 0em`, width: `100%`, padding: '2em 1em', textAlign: `right`, height: `auto`}}>
+						<TextField label="E-mail" name="email" required style={inputStyle} />
+						<TextField label="Message" name="message" multiline required style={inputStyle} />
+						<div style={{display: `flex`, alignItems: `center`}}>
+							<div style={{flex: `3`, height: `4em`, marginBottom: `1em`, background: `url(/img/captcha.png)`, backgroundSize: `contain`, backgroundRepeat: `no-repeat`}} 
+							/><div style={{flex: `1`}}><TextField name="captcha" required style={inputStyle} /></div>
+						</div>
+						<Button style={{width: `7em`}} variant='contained' color='primary' onClick={this.handleSubmitClick} disabled={this.state.submitButtonDisabled} type="submit">{this.state.submitLabel}</Button>
+					</Card>
+				</form>
     )
   }
 }
--- src/components/pages/Timeline.js
+++ src/components/pages/Timeline.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default {
   nodes: [
-    {title: "Zespół Szkół Elektryczno-Mechanicznych w Nowym Sączu", date: "2015", description: "", completed: true},
-    {title: "Started working in visperfect.com", date: "2015", description: "", completed: true},
-    {title: "Stop working in visperfect.com", date: "2017", description: "", completed: true},
-    {title: "Started studies at Cracow University of Technology", date: "2017", description: "", completed: true},
+    {title: <a rel="noopener noreferrer" target="_blank" href="http://zsem.edu.pl">ZSEM</a>, date: "2015", description: "", completed: true},
+    {title: <React.Fragment>Started working in <a rel="noopener noreferrer" target="_blank" href="http://visperfect.com">visperfect.com</a></React.Fragment>, date: "2015", description: "", completed: true},
+    {title: <React.Fragment>Stopped working in <a rel="noopener noreferrer" target="_blank" href="http://visperfect.com">visperfect.com</a></React.Fragment>, date: "2017", description: "", completed: true},
+    {title: <React.Fragment>Started studies at <a rel="noopener noreferrer" target="_blank" href="http://wieik.pk.edu.pl">Cracow University of Technology</a></React.Fragment>, date: "2017", description: "", completed: true},
   ]
 }
